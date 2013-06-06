73133b1c1e392b97254ceca56ad9150734c91792
Evan Moelter <evanmoelter@umail.ucsb.edu>
2013-06-05 22:18:43 -0700

    save state

--- src/edu/ucsb/cs56/projects/music/mp3_player/MusicPlayer.java
+++ src/edu/ucsb/cs56/projects/music/mp3_player/MusicPlayer.java
@@ -2,6 +2,8 @@ package edu.ucsb.cs56.projects.music.mp3_player;
 
 import java.io.*;
 
+import java.net.URL;
+
 import javax.media.*;
 import javax.media.Format;
 import javax.media.format.AudioFormat;
@@ -16,23 +18,6 @@ public class MusicPlayer{
 
     public static void play(String filename)
     {
-    	Format input1 = new AudioFormat(AudioFormat.MPEGLAYER3);
-		Format input2 = new AudioFormat(AudioFormat.MPEG);
-		Format output = new AudioFormat(AudioFormat.LINEAR);
-		PlugInManager.addPlugIn(
-			"com.sun.media.codec.audio.mp3.JavaDecoder",
-			new Format[]{input1, input2},
-			new Format[]{output},
-			PlugInManager.CODEC
-		);
-		try{
-			Player player = Manager.createPlayer(new MediaLocator(new File("resources/" + filename).toURI().toURL()));
-			player.start();
-		}
-		catch(Exception ex){
-			ex.printStackTrace();
-		}
-		System.out.println("done");
     }
     
 }
